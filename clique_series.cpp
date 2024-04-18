f635742de0a07db434b3092dda62a66b087ea22e
1van9 <90345212+1van9@users.noreply.github.com>
2024-04-18 21:47:35 +0300

    Update clique_series.cpp

--- clique_series.cpp
+++ clique_series.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 vec<Matrix> get_K11112(Frac n, Frac m) { 
-    // n != 0, n != m, n != -m
     Matrix A1 = {{n, n}, 
                  {n, m}};
     
@@ -27,9 +26,12 @@ vec<Matrix> get_K11112(Frac n, Frac m) {
 
 
 int main() {
-    int maxn = 20, maxm = 10;
+    // int maxn = 50, maxm = 5;
+    // int maxn = 100, maxm = 2;
+    int maxn = 20, maxm = 20;
+    bool ok = true;
     for (int n1 = 1; n1 < maxn; n1++) {
-        for (int m1 = 1; m1 < n1 && m1 < maxn; m1++) {
+        for (int m1 = 1; m1 < maxm; m1++) {
             if (m1 == n1)
                 continue;
             Frac f1 = make_frac(n1, 1);
@@ -37,10 +39,10 @@ int main() {
             vec<Matrix> k1 = get_K11112(f1, f2);
             k1.pop_back();
             for (int n2 = n1 + 1; n2 < maxn; n2++) {
-                for (int m2 = 1; m2 < n2 && m2 < maxm; m2++) {
+                for (int m2 = 1; m2 < maxm; m2++) {
                     if (n2 == m2)
                         continue;
-                    if (make_frac(n1, m1) == make_frac(n2, m2))
+                    if (make_frac(n1, m1) == make_frac(n2, m2) || make_frac(n1, m1) == make_frac(m2, n2))
                         continue;
                     Frac f3 = make_frac(n2, 1);
                     Frac f4 = make_frac(m2, 1);
@@ -50,20 +52,14 @@ int main() {
                     if (isomorphic_subgraphs_2_by_2(k1, k2)) {
                         cout << "Not coresopnds with hypothesis" << endl;
                         cout << n1 << " " << m1 << ", " << n2 << " " << m2 << endl; 
-                        for (auto el : k1) {
-                            cout << el;
-                        }
-                        cout << "----------------" << endl;
-                        for (auto el : k2) {
-                            cout << el;
-                        }
-                        return 0;
+                        ok = false;
                     }
                 }
             }
-            cout << "ok for " << n1 << " " << m1 << endl;
+            if (ok)
+                cout << "ok for " << n1 << " " << m1 << endl;
         }
     }
-    cout << "Evrything ok" << endl;
-    
+    if (ok)
+        cout << "Evrything ok" << endl;
 }
