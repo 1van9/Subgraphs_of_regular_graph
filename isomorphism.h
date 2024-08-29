4751cf2542d939328b3d07e2124cbca05ec53f12
1van9 <90345212+1van9@users.noreply.github.com>
2024-08-29 15:04:19 +0300

    Update isomorphism.h

--- isomorphism.h
+++ isomorphism.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#include "matrices.h"
+#include "matrix.h"
 #include <set>
 #include <algorithm>
 
-bool isomorphic_subgraphs_2_by_2(const vec<Matrix>& subg1, const vec<Matrix>& subg2) {
+template<typename T>
+bool isomorphic_subgraphs_2_by_2(const vec<Matrix<T>>& subg1, const vec<Matrix<T>>& subg2) {
     if (subg1.size() != subg2.size())
         return false;
     int n = subg1.size();
@@ -13,17 +14,17 @@ bool isomorphic_subgraphs_2_by_2(const vec<Matrix>& subg1, const vec<Matrix>& su
         p[i] = i;
     bool ok = false;
     // A_i = U B_{p_i} V
-    // U^-1 = B_{p_i} V (A_i)^-1   (1)
+    // U^{-1} = B_{p_i} V (A_i)^-1   (1)
 
     // V = (x y
     //      z t)
-    // Have n wasy, how to express U^-1, in term of x, y, z, t
-    std::set<Matrix> ways; // possible linear equasions on coefficients x, y, z, t
+    // Have n wasy, how to express U, in term of x, y, z, t
+    std::set<Matrix<T>> ways; // possible linear equasions on coefficients x, y, z, t
     do {
-        vec<Matrix> M; // express U^-1 coefficients in term of x, y, z, t 
+        vec<Matrix<T>> M; // express U coefficients in term of x, y, z, t 
         for (int i = 0; i < n; i++) {
-            Matrix A = subg2[p[i]], B = rev(subg1[i]); 
-            Matrix U_coefficients  = {
+            Matrix<T> A = subg2[p[i]], B = subg1[i].rev(); 
+            Matrix<T> U_coefficients  = {
                 {A[0][0] * B[0][0], A[0][0] * B[1][0], A[0][1] * B[0][0], A[0][1] * B[1][0]},
                 {A[1][0] * B[0][0], A[1][0] * B[1][0], A[1][1] * B[0][0], A[1][1] * B[1][0]},
                 {A[0][0] * B[0][1], A[0][0] * B[1][1], A[0][1] * B[0][1], A[0][1] * B[1][1]},
@@ -31,7 +32,7 @@ bool isomorphic_subgraphs_2_by_2(const vec<Matrix>& subg1, const vec<Matrix>& su
             }; 
             M.push_back(U_coefficients);
         }
-        Matrix linear_equasions((n - 1) * M[0].size(), vec<Frac>(4)); // equasions on x, y, z, t
+        Matrix<T> linear_equasions((n - 1) * M[0].size(), 4); // equasions on x, y, z, t
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < M[i].size(); j++) {
                 for (int k = 0; k < M[i][j].size(); k++) {
@@ -39,16 +40,16 @@ bool isomorphic_subgraphs_2_by_2(const vec<Matrix>& subg1, const vec<Matrix>& su
                     linear_equasions[(i - 1) * (M[i].size()) + j][k] = M[0][j][k] - M[i][j][k]; }
             }
         }
-        Gause(linear_equasions);
+        linear_equasions.Gause();
         ways.insert(linear_equasions);
     } while (next_permutation(p.begin(), p.end()));
 
     // the same for tansposition
     do {
-        vec<Matrix> M;
+        vec<Matrix<T>> M;
         for (int i = 0; i < n; i++) {
-            Matrix A = subg2[p[i]], B = rev(T(subg1[i]));
-            Matrix U_coefficients = {
+            Matrix<T> A = subg2[p[i]], B = (subg1[i].T()).Rev();
+            Matrix<T> U_coefficients = {
                 {A[0][0] * B[0][0], A[0][0] * B[1][0], A[0][1] * B[0][0], A[0][1] * B[1][0]},
                 {A[1][0] * B[0][0], A[1][0] * B[1][0], A[1][1] * B[0][0], A[1][1] * B[1][0]},
                 {A[0][0] * B[0][1], A[0][0] * B[1][1], A[0][1] * B[0][1], A[0][1] * B[1][1]},
@@ -56,7 +57,7 @@ bool isomorphic_subgraphs_2_by_2(const vec<Matrix>& subg1, const vec<Matrix>& su
             }; 
             M.push_back(U_coefficients);
         }
-        Matrix linear_equasions((n - 1) * M[0].size(), vec<Frac>(4));
+        Matrix<T> linear_equasions((n - 1) * M[0].size(), 4);
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < M[i].size(); j++) {
                 for (int k = 0; k < M[i][j].size(); k++) {
@@ -64,45 +65,45 @@ bool isomorphic_subgraphs_2_by_2(const vec<Matrix>& subg1, const vec<Matrix>& su
                 }
             }
         }
-        Gause(linear_equasions);
+        linear_equasions.Gause();
         ways.insert(linear_equasions);
     } while (next_permutation(p.begin(), p.end()));
 
     // check is there non zero solutions of x, y, z, t, which give V from GL_2
 
     for (auto lin_eq : ways) {
-        if (lin_eq[3][3].fi == lin_eq[3][3].se) {
+        if (lin_eq[3][3] == T(1)) {
             // 1 0 0 0
             // 0 1 0 0 
             // 0 0 1 0
             // 0 0 0 1
             continue;
         }
-        if (lin_eq[2][2].fi == 0 && lin_eq[2][3].fi == lin_eq[2][3].se) {
+        if (!lin_eq[2][2] && lin_eq[2][3] == T(1)) {
             //     0 0           0
             // 0 0 1 0     0 1 * 0
             // 0 0 0 1     0 0 0 1
-            if (lin_eq[1][2].fi == lin_eq[1][2].se && lin_eq[1][1].fi == 0)
+            if (lin_eq[1][2] == T(1) && !lin_eq[1][1])
                 continue;
-            if (lin_eq[1][2].fi == 0)
+            if (!lin_eq[1][2])
                 continue;
             return true;
         }
-        if (lin_eq[2][2].fi == lin_eq[2][2].se) {
+        if (lin_eq[2][2] == T(1)) {
             // 1 0 0 *
             // 0 1 0 *
             // 0 0 1 *
-            Frac det = lin_eq[0][3] - lin_eq[1][3] * lin_eq[2][3];
-            if (det.fi != 0) {
+            T det = lin_eq[0][3] - lin_eq[1][3] * lin_eq[2][3];
+            if (det) {
                 return true;
             }
             continue;
         }
-        if (lin_eq[0][0].fi == 0) {
+        if (!lin_eq[0][0]) {
             // 0 1 0 *     0 1 * 0     0 1 * *     0 0 1 0     0 0 1 0    0 0 0 1    0 0 0 0
             // 0 0 1 *     0 0 0 1     0 0 0 0     0 0 0 1     0 0 0 0    0 0 0 0    0 0 0 0
-            if (lin_eq[1][2].fi == 0 && lin_eq[1][3].fi == lin_eq[1][3].se) {
-                if (lin_eq[0][1].fi == lin_eq[0][1].se && lin_eq[0][2].fi != 0) {
+            if (!lin_eq[1][2] && lin_eq[1][3] == T(1)) {
+                if (lin_eq[0][1] == T(1) && !lin_eq[0][2]) {
                     return true;
                 }
                 continue;
@@ -111,18 +112,18 @@ bool isomorphic_subgraphs_2_by_2(const vec<Matrix>& subg1, const vec<Matrix>& su
         }
         // 1 0 * *     1 * 0 *     1 * * 0   1 * * *
         // 0 1 * *     0 0 1 *     0 0 0 1   0 0 0 0
-        if (lin_eq[1][1].fi == lin_eq[1][1].se) {
+        if (lin_eq[1][1] == T(1)) {
             // 1 0 * *
             // 0 1 * * 
-            if (lin_eq[0][3].fi == 0 && lin_eq[1][2].fi == 0 && lin_eq[0][2] == lin_eq[1][3]) {
+            if (!lin_eq[0][3] && !lin_eq[1][2] && lin_eq[0][2] == lin_eq[1][3]) {
                 continue;
             }
             return true;
         }
-        if (lin_eq[1][2].fi == lin_eq[1][2].se) {
+        if (lin_eq[1][2] == T(1)) {
             // 1 * 0 *
             // 0 0 1 *
-            if (lin_eq[0][1].fi == 0 && (lin_eq[0][3] == lin_eq[1][3])) {
+            if (!lin_eq[0][1] && (lin_eq[0][3] == lin_eq[1][3])) {
                 continue;
             }
             return true;
